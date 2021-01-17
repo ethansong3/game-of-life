f725b3ea7b61bf0541814a8543fff260a63f7d3c
Ethan <ersong@uci.edu>
2021-01-16 18:00:06 -0800

    hello world

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>OHello, world!</Text>
+      <Text>Hello, world!</Text>
       <StatusBar style="auto" />
     </View>
   );
